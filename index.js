5f48f1ab021e83a9c34e844bcb5ae57b817e34f4
elseich <elseich@langrre.ddns.net>
2021-06-28 15:39:26 +0900

    redis session test

--- index.js
+++ index.js
@@ -2,13 +2,75 @@
 'use stricts';
 
 const express = require('express');
-const app = express();
-
-app.get('/', function(req, res) {
-  res.send('hello?');
-});
+const session = require('express-session');
+const redis = require('redis');
+const redisStore = require('connect-redis')(session);
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`listening ... port(${port})`);
-})
+
+function server() {
+  global.app = express();
+  global.app.redisClient = redis.createClient();
+
+  app.use(session({
+    store : new redisStore({client : global.app.redisClient}),
+    saveUninitialized : false,
+    secret : 'namannothingcat',
+    resave : false,
+  }));
+
+  app.use((req, res, next) => {
+    if (!req.session) {
+      return next(new Error('oh no'));
+    }
+
+    next();
+  })
+
+  app.get('/', (req, res) => {
+    res.send('What do you want?!?');
+  });
+
+  app.get('/session-test', (req, res) => {
+    res.send('not yet session id');
+  });
+
+  app.get('/redis-store-count', (req, res) => {
+    console.log(`req.session.count = ${req.session.count}`);
+    let session = req.session;
+    if (session && session.count) {
+      session.count++;
+    }
+    else {
+      session.count = 1;
+    }
+    res.send('count is ' + session.count + ' ' + session.id);
+  })
+
+  app.get('/set-extradata', (req, res) => {
+    req.session.extradata = {
+      a : 10,
+      b : 15, 
+      c : 'extradata 다'
+    };
+
+    res.send(200);
+  })
+
+  app.get('/show-session', (req, res) => {
+    console.log(req.session.extradata);
+
+    res.send(200);
+  })
+
+  app.get('/session-destroy', (req, res) => {
+    req.session.destroy();
+    res.send('session destroyed!');
+  })
+  
+  app.listen(port, () => {
+    console.log(`listening ... port(${port})`);
+  })
+}
+
+server();
